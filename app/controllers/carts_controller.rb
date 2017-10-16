4c136b5a8d803885f0665e456a4e0a97493bde22
Chase Paules <cpaules@gmail.com>
2017-10-16 15:12:56 +0000

    Done.

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,3 +1,16 @@
+require 'pry'
 class CartsController < ApplicationController
+  before_action :user_signed_in?
+
+  def show
+    @cart = Cart.find(params[:id])
+  end
+
+  def checkout
+    #binding.pry
+    @cart = Cart.find(params[:id])
+    @cart.checkout
+    redirect_to cart_path(@cart)
+  end
 
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,3 +1,7 @@
 class LineItem < ActiveRecord::Base
+  belongs_to :item
+  belongs_to :cart
 
+  validates :cart_id, presence: true
+  validates :item_id, presence: true
 end
